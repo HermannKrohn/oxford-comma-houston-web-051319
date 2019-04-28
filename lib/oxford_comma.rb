164b1bccbdb62c121f23c3ff0c3548b63da4c1e4
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-04-28 18:32:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
 def oxford_comma(array)
-  lowerHalf = []
+  newArr = []
   i=0 
   while i < array.length-1 
-    
+    newArr << array[i]
+    i += 1 
+  end
+  newArr << "and #{array[-1]}"
+  newArr.join
 end
