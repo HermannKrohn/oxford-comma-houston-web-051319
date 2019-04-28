0692097b2f6dc3dd51c138a15b0ac121b8b20276
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-04-28 18:36:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,14 @@
 def oxford_comma(array)
-  newArr = []
-  i=0 
-  while i < array.length-1 
-    newArr << array[i]
-    i += 1 
+  if array.length == 1 
+    array.join
+  else
+    newArr = []
+    i=0 
+    while i < array.length-1 
+      newArr << "#{array[i]}, "
+      i += 1 
+    end
+    newArr << " and #{array[-1]}"
+    newArr.join
   end
-  newArr << " and #{array[-1]}"
-  newArr.join
 end
